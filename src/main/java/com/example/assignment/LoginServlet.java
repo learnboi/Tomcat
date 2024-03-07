dd33aac48343d49268d42edccbe2978bd4947f8f
learnboi <chaitanyagayakwad100@gmail.com>
2024-03-08 00:32:22 +0530

    upload

--- src/main/java/com/example/assignment/LoginServlet.java
+++ src/main/java/com/example/assignment/LoginServlet.java
@@ -1,17 +1,16 @@
 package com.example.assignment;
 
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
+import java.util.List;
 
 import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet("/login")
-public class HelloServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
         response.setContentType("text/html");
         String Username = request.getParameter("username");
@@ -19,7 +18,15 @@ public class HelloServlet extends HttpServlet {
         PrintWriter pw = response.getWriter();
         RequestDispatcher rd;
         if(Username.equals("admin") && Password.equals("admin")){
-            pw.write("<h1>Hello " + Password + "</h1>");
+            EmployeeData ed = new EmployeeData();
+            try {
+                List<Employee> employeeList = ed.fetchemployeedata();
+                request.setAttribute("list",employeeList);
+                rd = request.getRequestDispatcher("Employeedata.jsp");
+                rd.forward(request,response);
+            } catch (Exception e) {
+                System.out.println(e.getLocalizedMessage());
+            }
         } else{
             try {
                 String url ="jdbc:mysql://localhost:3306/login";
@@ -34,7 +41,7 @@ public class HelloServlet extends HttpServlet {
                     if (resultSet.next()) {
                         String storedPassword = resultSet.getString("password");
                         if (Password.equals(storedPassword)) {
-                            rd = request.getRequestDispatcher("/Welcome.html");
+                            rd = request.getRequestDispatcher("/Welcome.jsp");
                             rd.forward(request,response);
                         } else {
                             rd = request.getRequestDispatcher("/Error.html");
